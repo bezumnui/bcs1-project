4e97f4e67a1a2fbf3a8a3ebd1d65f5003a21b65c
haris-s-s <harisejaz616@gmail.com>
2025-04-27 20:30:31 +0400

    Update gallery.py

--- gallery.py
+++ gallery.py
@@ -1,5 +1,7 @@
-from PyQt6.QtWidgets import (QDialog, QGridLayout, QVBoxLayout, QComboBox, QFrame)
 
+from PyQt6.QtWidgets import QApplication, QDialog, QVBoxLayout, QComboBox, QFrame, QLabel, QGridLayout, QHBoxLayout, QPushButton
+from PyQt6.QtGui import QIcon
+from PyQt6.QtCore import Qt
 from sub_applications.bin_hex_octal_converter import BinHexOctalConverterApplication
 from sub_applications.factorial_application import FactorialApplication
 from sub_applications.ieee_754_converter_application import IEEE754ConverterApplication
@@ -18,36 +20,43 @@ class WidgetGallery(QDialog):
     def __init__(self, parent=None):
         super(WidgetGallery, self).__init__(parent)
 
-        self.converter_layout = QGridLayout()
-        self.student_manager = QGridLayout()
+        self.main_layout = QVBoxLayout()
 
-        self.main_frame = QFrame()
+        self.top_layout = QHBoxLayout()
+
+        self.mode_combo_box = QComboBox()
+        self.mode_combo_box.addItems([MainMenuCombo.NUMBER_CONVERTER, MainMenuCombo.IEEE, MainMenuCombo.FACTORIAL,
+                                      MainMenuCombo.STUDENT_MANAGER])
+        self.mode_combo_box.setStyleSheet("background-color: #444444; color: white; padding: 5px;")
+        self.mode_combo_box.textActivated.connect(self.change_mode)
+        self.top_layout.addWidget(self.mode_combo_box)
+
+        self.content_frame = QFrame()
+        self.content_layout = QVBoxLayout(self.content_frame)
 
         self.bin_hex_octal_converter_application = BinHexOctalConverterApplication()
         self.ieee_754_application = IEEE754ConverterApplication()
         self.factorial_application = FactorialApplication()
         self.student_manager_application = StudentManagerApplication()
 
-        self.top_layout = QVBoxLayout()
-        mode_combo_box = QComboBox()
-        mode_combo_box.addItems([MainMenuCombo.NUMBER_CONVERTER, MainMenuCombo.IEEE, MainMenuCombo.FACTORIAL,
-                                 MainMenuCombo.STUDENT_MANAGER])
-        mode_combo_box.textActivated.connect(self.change_mode)
-        self.top_layout.addWidget(mode_combo_box)
+        self.bin_hex_octal_converter_application.register_widget(self.content_layout)
+        self.ieee_754_application.register_widget(self.content_layout)
+        self.factorial_application.register_widget(self.content_layout)
+        self.student_manager_application.register_widget(self.content_layout)
 
-        self.ieee_754_application.register_widget(self.top_layout)
-        self.bin_hex_octal_converter_application.register_widget(self.top_layout)
-        self.factorial_application.register_widget(self.top_layout)
-        self.student_manager_application.register_widget(self.top_layout)
+        self.main_layout.addLayout(self.top_layout)
+        self.main_layout.addWidget(self.content_frame)
 
-        self.setLayout(self.top_layout)
+        self.setLayout(self.main_layout)
 
         self.current_application = self.bin_hex_octal_converter_application
         self.setWindowTitle(self.current_application.get_name())
         self.current_application.display()
 
-    def resize_window(self):
-        self.setFixedSize(self.baseSize())
+        self.setWindowTitle("Enhanced Application")
+        self.setWindowIcon(QIcon("img/icon.png")) 
+        self.resize(900, 700)
+        self.setStyleSheet("background-color: #222222; color: white;")
 
     def replace_application(self, new_app: SubApplication):
         self.current_application.exit()
@@ -56,7 +65,6 @@ class WidgetGallery(QDialog):
         self.setWindowTitle(self.current_application.get_name())
 
     def change_mode(self, data):
-
         match data:
             case MainMenuCombo.NUMBER_CONVERTER:
                 self.replace_application(self.bin_hex_octal_converter_application)
